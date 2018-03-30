b0d519001e2572b6638cbf7b5c718bfc4b182e93
Colm Hughes <cjhugo1991@hotmail.com>
2018-03-30 19:53:49 +0000

    Postgres db

--- irish_photos/settings/base.py
+++ irish_photos/settings/base.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['irish-photos-django-colmhughes.c9users.io']
+ALLOWED_HOSTS = ['irish-photos-django-colmhughes.c9users.io', 'photos-project3.herokuapp.com']
 
 
 # Application definition
@@ -79,10 +79,7 @@ WSGI_APPLICATION = 'irish_photos.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'))
 }
 
 
